48442c85aee974571d00b646a459185533fa1eb7
Ali Saleh <alisheikh1114@gmail.com>
2018-08-28 02:23:06 +0500

    Added corsheaders

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -95,6 +95,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
