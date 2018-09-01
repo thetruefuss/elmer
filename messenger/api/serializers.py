dfe4099e2a4260d863c9524b5beff1b52a3d2699
Ali Saleh <alisheikh1114@gmail.com>
2018-09-01 17:44:11 +0500

    Updated the serializers

--- messenger/api/serializers.py
+++ messenger/api/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.contrib.humanize.templatetags.humanize import naturaltime
 
 from messenger.models import Message
 from rest_framework import serializers
@@ -6,31 +7,56 @@ from user_accounts.api.serializers import UserDetailSerializer
 
 
 class ContactsListSerializer(serializers.ModelSerializer):
+    """
+    Serializer that represents a users contact list.
+    """
+
     screen_name = serializers.SerializerMethodField()
     profile_picture = serializers.SerializerMethodField()
-    url_to = serializers.SerializerMethodField()
 
     class Meta:
         model = User
         fields = [
-            'screen_name', 'profile_picture', 'url_to',
+            'id', 'username', 'screen_name', 'profile_picture',
         ]
 
     def get_screen_name(self, obj):
+        """
+        Returns user screen name.
+
+        :return: string
+        """
         return obj.profile.screen_name()
 
     def get_profile_picture(self, obj):
-        return obj.profile.get_picture()
+        """
+        Returns user's profile picture link.
 
-    def get_url_to(self, obj):
-        return "/api/messages/chat/?username={}".format(obj.username)
+        :return: string
+        """
+        request = self.context.get('request')
+        profile_picture_url = obj.profile.get_picture()
+        return request.build_absolute_uri(profile_picture_url)
 
 
 class MessageListSerializer(serializers.ModelSerializer):
-    from_user = UserDetailSerializer(read_only=True)
+    """
+    Serializer that represents messages.
+    """
+
+    from_user = ContactsListSerializer(read_only=True)
+    data_naturaltime = serializers.SerializerMethodField()
 
     class Meta:
         model = Message
         fields = [
-            'from_user', 'message', 'date',
+            'id', 'from_user', 'message', 'date', 'data_naturaltime',
         ]
+
+    def get_data_naturaltime(self, obj):
+        """
+        Returns human readable time.
+
+        :return: string
+        """
+        return naturaltime(obj.date)
