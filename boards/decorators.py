7aa097c84a8d86402fe0d28aa645f8fdc724ff64
Ali Saleh <alisheikh1114@gmail.com>
2018-09-30 02:29:21 +0500

    Added boards decorators

---
+++ boards/decorators.py
@@ -0,0 +1,29 @@
+from django.core.exceptions import PermissionDenied
+
+from .models import Board
+
+
+def user_is_board_admin(f):
+    def wrap(request, *args, **kwargs):
+        board = Board.objects.get(slug=kwargs['board'])
+        if request.user in board.admins.all():
+            return f(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+
+    wrap.__doc__ = f.__doc__
+    wrap.__name__ = f.__name__
+    return wrap
+
+
+def user_is_not_banned_from_board(f):
+    def wrap(request, *args, **kwargs):
+        board = Board.objects.get(slug=kwargs['board'])
+        if not request.user in board.banned_users.all():
+            return f(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+
+    wrap.__doc__ = f.__doc__
+    wrap.__name__ = f.__name__
+    return wrap
--- boards/views.py
+++ boards/views.py
@@ -14,6 +14,7 @@ from mysite.decorators import ajax_required
 from subjects.models import Subject
 from utils import check_image_extension
 
+from .decorators import user_is_board_admin, user_is_not_banned_from_board
 from .forms import BoardForm
 from .models import Board
 
@@ -27,106 +28,6 @@ class BoardsPageView(ListView):
     paginate_by = 20
     template_name = 'boards/view_all_boards.html'
     context_object_name = 'boards'
-
-
-@login_required
-def banned_users(request, board):
-    """
-    Displays a list of banned users to the board admins.
-    """
-    board = get_object_or_404(Board,
-                              slug=board)
-    admins = board.admins.all()
-    if request.user in admins:
-        users = board.banned_users.all()
-        bv = True
-
-        paginator = Paginator(users, 20)
-        page = request.GET.get('page')
-        if paginator.num_pages > 1:
-            p = True
-        else:
-            p = False
-        try:
-            users = paginator.page(page)
-
-        except PageNotAnInteger:
-            users = paginator.page(1)
-
-        except EmptyPage:
-            users = paginator.page(paginator.num_pages)
-
-        p_obj = users
-
-        return render(request, 'boards/banned_users.html', {
-            'board': board,
-            'bv': bv,
-            'page': page,
-            'p_obj': p_obj,
-            'p': p,
-            'users': users
-        })
-    else:
-        return redirect('home')
-
-
-@login_required
-def ban_user(request, board, user_id):
-    """
-    Handles requests from board admins to ban users form the board.
-    """
-    board = get_object_or_404(Board,
-                              slug=board)
-    if request.user in board.admins.all():
-        user = get_object_or_404(User,
-                                 id=user_id)
-        if board in user.subscribed_boards.all():
-            board.subscribers.remove(user)
-            board.banned_users.add(user)
-            return redirect('banned_users', board=board.slug)
-        else:
-            board.banned_users.remove(user)
-            return redirect('banned_users', board=board.slug)
-    else:
-        return redirect('home')
-
-
-class UserSubscriptionListView(ListView):
-    """
-    Basic ListView implementation to call the subscriptions list per user.
-    """
-    model = Board
-    paginate_by = 10
-    template_name = 'boards/user_subscription_list.html'
-    context_object_name = 'subscriptions'
-
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super().dispatch(*args, **kwargs)
-
-    def get_queryset(self, **kwargs):
-        user = get_object_or_404(User,
-                                 username=self.request.user)
-        return user.subscribed_boards.all()
-
-
-class UserCreatedBoardsPageView(ListView):
-    """
-    Basic ListView implementation to call the boards list per user.
-    """
-    model = Board
-    paginate_by = 20
-    template_name = 'boards/user_created_boards.html'
-    context_object_name = 'user_boards'
-
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super().dispatch(*args, **kwargs)
-
-    def get_queryset(self, **kwargs):
-        user = get_object_or_404(User,
-                                 username=self.request.user)
-        return user.inspected_boards.all()
 
 
 class BoardPageView(ListView):
@@ -149,6 +50,55 @@ class BoardPageView(ListView):
         context["admins"] = self.board.admins.all()
         context["board"] = self.board
         return context
+
+
+@method_decorator(login_required, name='dispatch')
+class UserSubscriptionListView(ListView):
+    """
+    Basic ListView implementation to call the subscriptions list per user.
+    """
+    model = Board
+    paginate_by = 10
+    template_name = 'boards/user_subscription_list.html'
+    context_object_name = 'subscriptions'
+
+    def get_queryset(self, **kwargs):
+        user = get_object_or_404(User,
+                                 username=self.request.user)
+        return user.subscribed_boards.all()
+
+
+@login_required
+@ajax_required
+@user_is_not_banned_from_board
+def subscribe(request, board):
+    """
+    Subscribes a board & returns subscribers count.
+    """
+    board = get_object_or_404(Board,
+                              slug=board)
+    user = request.user
+    if board in user.subscribed_boards.all():
+        board.subscribers.remove(user)
+    else:
+        board.subscribers.add(user)
+    return HttpResponse(board.subscribers.count())
+
+
+@method_decorator(login_required, name='dispatch')
+class UserCreatedBoardsPageView(ListView):
+    """
+    Basic ListView implementation to call the boards list per user.
+    """
+    model = Board
+    paginate_by = 20
+    template_name = 'boards/user_created_boards.html'
+    context_object_name = 'user_boards'
+
+    def get_queryset(self, **kwargs):
+        user = get_object_or_404(User,
+                                 username=self.request.user)
+        return user.inspected_boards.all()
 
 
 @login_required
@@ -174,10 +124,8 @@ def new_board(request):
 
             if result['success']:
                 new_board = board_form.save()
-                first_admin = request.user
-                new_board.admins.add(first_admin)
-                new_board.subscribers.add(first_admin)
-                new_board.save()
+                new_board.admins.add(request.user)
+                new_board.subscribers.add(request.user)
                 return redirect(new_board.get_absolute_url())
         else:
             board_form = BoardForm()
@@ -192,43 +140,78 @@ def new_board(request):
 
 
 @login_required
-@ajax_required
-def subscribe(request, board):
+@user_is_board_admin
+def edit_board_cover(request, board):
     """
-    Subscribes a board & returns subscribers count.
+    Displays edit form for board cover and handles edit action.
     """
     board = get_object_or_404(Board,
                               slug=board)
-    user = request.user
-    if board in user.subscribed_boards.all():
-        board.subscribers.remove(user)
+    if request.method == 'POST':
+        board_cover = request.FILES.get('cover')
+        if check_image_extension(board_cover.name):
+            board.cover = board_cover
+            board.save()
+            return redirect('board', board=board.slug)
+        else:
+            return HttpResponse('Filetype not supported. Supported filetypes are .jpg, .png etc.')
     else:
-        board.subscribers.add(user)
-    return HttpResponse(board.subscribers.count())
+        form_filling = True
+        return render(request, 'boards/edit_board_cover.html', {
+            'board': board, 'form_filling': form_filling
+        })
 
 
 @login_required
-def edit_board_cover(request, board):
+@user_is_board_admin
+def banned_users(request, board):
     """
-    Displays edit from for board cover and handles edit action.
+    Displays a list of banned users to the board admins.
     """
     board = get_object_or_404(Board,
                               slug=board)
-    admins = board.admins.all()
-    if request.user in admins:
-        if request.method == 'POST':
-            board_cover = request.FILES.get('cover')
+    users = board.banned_users.all()
 
-            if check_image_extension(board_cover.name):
-                board.cover = board_cover
-                board.save()
-                return redirect('board', board=board.slug)
-            else:
-                return HttpResponse('Filetype not supported. Supported filetypes are .jpg, .png etc.')
-        else:
-            form_filling = True
-            return render(request, 'boards/edit_board_cover.html', {
-                'board': board, 'form_filling': form_filling
-            })
+    paginator = Paginator(users, 20)
+    page = request.GET.get('page')
+    if paginator.num_pages > 1:
+        p = True
     else:
-        return HttpResponse('You can not change the cover of this board unless you are an admin here.')
+        p = False
+    try:
+        users = paginator.page(page)
+    except PageNotAnInteger:
+        users = paginator.page(1)
+    except EmptyPage:
+        users = paginator.page(paginator.num_pages)
+
+    p_obj = users
+    bv = True
+
+    return render(request, 'boards/banned_users.html', {
+        'board': board,
+        'bv': bv,
+        'page': page,
+        'p_obj': p_obj,
+        'p': p,
+        'users': users
+    })
+
+
+@login_required
+@user_is_board_admin
+def ban_user(request, board, user_id):
+    """
+    Handles requests from board admins to ban users from the board.
+    """
+    board = get_object_or_404(Board,
+                              slug=board)
+    user = get_object_or_404(User,
+                             id=user_id)
+    if board in user.subscribed_boards.all():
+        board.subscribers.remove(user)
+        board.banned_users.add(user)
+        return redirect('banned_users', board=board.slug)
+    else:
+        board.banned_users.remove(user)
+        return redirect('banned_users', board=board.slug)
